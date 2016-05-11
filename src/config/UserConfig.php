152821fee251b64b37903d3b1979c4563ac67334
cdcchen <cdcchen@gmail.com>
2016-05-11 11:56:23 +0800

    Add feature: get data from cache.

--- src/config/UserConfig.php
+++ src/config/UserConfig.php
@@ -10,11 +10,16 @@ namespace yiiplus\config;
 
 
 use yii\base\Component;
+use yii\base\InvalidConfigException;
 use yii\caching\Cache;
 use yii\db\Connection;
-use yii\di\Instance;
 use yii\db\Query;
+use yii\di\Instance;
 
+/**
+ * Class UserConfig
+ * @package yiiplus\config
+ */
 class UserConfig extends Component
 {
     /**
@@ -22,6 +27,9 @@ class UserConfig extends Component
      */
     public $db = 'db';
 
+    /**
+     * @var string
+     */
     public $table = '{{%user_config}}';
 
     /**
@@ -29,55 +37,174 @@ class UserConfig extends Component
      */
     public $cache = 'cache';
 
+    /**
+     * @throws InvalidConfigException
+     */
     public function init()
     {
         parent::init();
         $this->db = Instance::ensure($this->db, Connection::className());
+        if (!($this->db instanceof Connection)) {
+            throw new InvalidConfigException('UserConfig::db is required.');
+        }
+
         if ($this->cache !== null) {
             $this->cache = Instance::ensure($this->cache, Cache::className());
         }
     }
 
-    public function get($userID, $key, $defaultValue = null)
+    /**
+     * @param int|string $userId
+     * @param string $key
+     * @param mixed $defaultValue
+     * @return mixed
+     */
+    public function get($userId, $key, $defaultValue = null)
     {
         static $values = [];
-        $cacheKey = $key . $userID;
+        $cacheKey = $this->buildCacheKey($userId, $key);
 
-        if (isset($values[$cacheKey]))
-            return $values[$cacheKey];
+        if (!isset($values[$cacheKey])) {
+            $value = $this->getFromCache($userId, $key);
+            if ($value === false) {
+                $value = $this->getFromDb($userId, $key);
+            }
 
-        $value = (new Query())
-            ->from([$this->table])
-            ->select(['value'])
-            ->where([
-                'key' => $key,
-                'user_id' => $userID
-            ])->scalar($this->db);
+            $values[$cacheKey] = $value === false ? $defaultValue : $value;
+        }
 
-        $values[$cacheKey] = $value === false ? $defaultValue : $value;
         return $values[$cacheKey];
     }
 
-    public function set($userID, $key, $value)
+    /**
+     * @param int|string $userId
+     * @param string $key
+     * @param mixed $value
+     * @return int
+     * @throws \yii\db\Exception
+     */
+    public function set($userId, $key, $value)
     {
-        if ($this->get($userID, $key) === null)
-            return $this->add($userID, $key, $value);
-        else
-            return $this->db->createCommand()->update($this->table,
-                ['value' => $value],
-                [
-                    'key' => $key,
-                    'user_id' => $userID
-                ]
-            )->execute();
+        if ($this->get($userId, $key) === null) {
+            return $this->add($userId, $key, $value);
+        } else {
+            $condition = ['key' => $key, 'user_id' => $userId];
+            $affectedRows = $this->db->createCommand()
+                                     ->update($this->table, ['value' => $value], $condition)
+                                     ->execute();
+
+            if ($this->cacheIsActive() && $affectedRows) {
+                $this->deleteFromCache($userId, $key);
+            }
+
+            return $affectedRows;
+        }
     }
 
-    public function add($userID, $key, $value)
+    /**
+     * @param int|string $userId
+     * @param string $key
+     * @param mixed $value
+     * @return int
+     * @throws \yii\db\Exception
+     */
+    public function add($userId, $key, $value)
     {
-        return $this->db->createCommand()->insert($this->table, [
-            'user_id' => $userID,
+        $columns = [
+            'user_id' => $userId,
             'key' => $key,
             'value' => $value
-        ])->execute();
+        ];
+
+        return $this->db->createCommand()->insert($this->table, $columns)->execute();
+    }
+
+    /**
+     * @param int|string $userId
+     * @param string $key
+     * @return bool
+     * @throws \yii\db\Exception
+     */
+    public function delete($userId, $key)
+    {
+        $transaction = $this->db->beginTransaction();
+
+        $result = $this->deleteFromDb($userId, $key) && $this->deleteFromCache($userId, $key);
+        if ($result) {
+            $transaction->commit();
+        } else {
+            $transaction->rollBack();
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param int|string $userId
+     * @param string $key
+     * @return bool|mixed
+     */
+    protected function getFromCache($userId, $key)
+    {
+        if ($this->cacheIsActive()) {
+            return $this->cache->get($this->buildCacheKey($userId, $key));
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @param int|string $userId
+     * @param string $key
+     * @return bool|string
+     */
+    protected function getFromDb($userId, $key)
+    {
+        $condition = ['user_id' => $userId, 'key' => $key];
+
+        return (new Query())
+            ->from([$this->table])
+            ->select(['value'])
+            ->where($condition)
+            ->scalar($this->db);
+    }
+
+    /**
+     * @param int|string $userId
+     * @param string $key
+     * @return \yii\db\Command
+     */
+    protected function deleteFromDb($userId, $key)
+    {
+        $condition = ['user_id' => $userId, 'key' => $key];
+        return $this->db->createCommand()->delete($this->table, $condition);
+    }
+
+    /**
+     * @param int|string $userId
+     * @param string $key
+     * @return bool
+     */
+    protected function deleteFromCache($userId, $key)
+    {
+        return $this->cache->delete($this->buildCacheKey($userId, $key));
+    }
+
+    /**
+     * @return bool
+     */
+    protected function cacheIsActive()
+    {
+        return $this->cache instanceof Cache;
+    }
+
+    /**
+     * @param int|string $userId
+     * @param string $key
+     * @return string
+     */
+    protected function buildCacheKey($userId, $key)
+    {
+        return $key . '_' . $userId;
     }
 }
